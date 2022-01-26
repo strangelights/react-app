d2f7dfe8b85f9f2389296b9ca9cb3337447ebe88
Josh Brookshire <jbrookshire7@gmail.com>
2022-01-26 18:36:01 -0500

    Add Affirmation component

---
+++ src/components/Affirmation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Affirmation(props) {
+    
+    return (
+        <div className="affirmation-text">
+            <p>{props.text}</p>
+        </div>
+    )
+};
+
+export  default Affirmation;
