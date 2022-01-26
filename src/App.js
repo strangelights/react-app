35de675aa4b479bb1b16ba724605aefb48c6bf32
Josh Brookshire <jbrookshire7@gmail.com>
2022-01-25 22:15:02 -0500

    Test Heroku auto-deploy

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ const App = () => {
     <div className="App">
       <h2>My React App</h2>
       <h3>Hello Whirrled</h3>
+      <h5>Test Heroku Deploy</h5>
     </div>
   );
 }
 
 export default App;
+ 
