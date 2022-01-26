0a78950fbfb09473afb9f10a16c0db5cf05b8cac
Josh Brookshire <jbrookshire7@gmail.com>
2022-01-25 23:54:31 -0500

    Add Axios and set up Shutterstock API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import { BASE_URL } from './globals';
 import './styles/App.css';
 
 const App = () => {
-  return (
-    <div className="App">
-      <h2>My React App</h2>
-      <h3>Hello Whirrled</h3>
-      <h5>Test Heroku Deploy</h5>
-    </div>
-  );
+
+    const username = process.env.REACT_APP_SHUTTERSTOCK_KEY;
+    const password = process.env.REACT_APP_SHUTTERSTOCK_SECRET;
+
+    useEffect(() => {
+        async function getStockPhoto() {
+            const res = await axios.get(`${BASE_URL}/v2/images/search`, {
+                params:{
+                    query: "affirmation"
+                },
+                auth: {
+                  username: username,
+                  password: password
+                }
+            });
+            console.log(res); 
+        }
+        getStockPhoto();
+    }, []);
+
+
+    return (
+        <div className="App">
+        <h2>Stock Affirmations</h2>
+        <h3>When you just need that extra pick me up.</h3>
+        </div>
+    );
 }
 
 export default App;
