9a351ed3e71841bc862ae921119192731460de8d
Josh Brookshire <jbrookshire7@gmail.com>
2022-01-26 01:11:59 -0500

    Add affirmation API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { BASE_URL } from './globals';
+import { SS_BASE_URL, AFF_BASE_URL } from './globals';
 import './styles/App.css';
 
 const App = () => {
@@ -9,8 +9,9 @@ const App = () => {
     const password = process.env.REACT_APP_SHUTTERSTOCK_SECRET;
 
     useEffect(() => {
+        
         async function getStockPhoto() {
-            const res = await axios.get(`${BASE_URL}/v2/images/search`, {
+            const res = await axios.get(`${SS_BASE_URL}/v2/images/search`, {
                 params:{
                     query: "affirmation"
                 },
@@ -21,9 +22,15 @@ const App = () => {
             });
             console.log(res); 
         }
-        getStockPhoto();
-    }, []);
 
+        async function getAffirmation() {
+            const res = await axios.get(AFF_BASE_URL);
+            console.log(res.data[0].phrase[1].toUpperCase() + res.data[0].phrase.substring(2)); 
+        }
+
+        getStockPhoto();
+        getAffirmation();
+    }, []);
 
     return (
         <div className="App">
--- src/globals.js
+++ src/globals.js
@@ -1,1 +1,3 @@
-export const BASE_URL = 'https://api.shutterstock.com';
+export const SS_BASE_URL = 'https://api.shutterstock.com';
+
+export const AFF_BASE_URL = 'https://dulce-affirmations-api.herokuapp.com/affirmation';
